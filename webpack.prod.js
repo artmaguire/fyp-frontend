bea0187dccad45ad8bb60b61da841aae58d6cfaf
Art Maguire <a.elnino11@gmail.com>
2021-04-09 15:57:01 +0100

    Prevent LICENSE.txt from being generated

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,11 @@
-const glob = require('glob')
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const PurgeCSSPlugin = require('purgecss-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const glob = require('glob');
 
 const PATHS = {
   src: __dirname + '/src',
@@ -17,6 +18,13 @@ module.exports = merge(common, {
     minimizer: [
       `...`,
       new CssMinimizerPlugin(),
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      })
     ],
     splitChunks: {
       chunks: 'all'
