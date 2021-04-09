24753f75b488f1e79abecb813eb94c8c0d59f364
Art Maguire <a.elnino11@gmail.com>
2021-04-09 15:34:54 +0100

    Add compression plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,8 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 const PATHS = {
   src: __dirname + '/src',
@@ -19,10 +20,13 @@ module.exports = merge(common, {
     ],
     splitChunks: {
       chunks: 'all'
-    }
+    },
+    runtimeChunk: true,
+    usedExports: true
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new CompressionWebpackPlugin(),
     new PurgeCSSPlugin({
       paths: glob.sync(`${ PATHS.src }/**/*`, { nodir: true }),
       safelist: {
